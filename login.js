c3942abfe0cc30f976a2944a2b1e9aec40ea8f82
mahalekshmim <41044027+mahalekshmim@users.noreply.github.com>
2018-12-14 12:37:21 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+function validate form(){
+x=document get Element By Id("email").value
+if(x=="){ 
+alert("email must be filled");
+return.false
+}
+x=document get Element By Id("password").value
+if(x=="){ 
+alert("password must be filled");
+return.false
+}
+}
