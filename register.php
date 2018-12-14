4d494d84d1bf9b8bf113f4f45b20b4cb09447d4c
mahalekshmim <41044027+mahalekshmim@users.noreply.github.com>
2018-12-14 12:39:49 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php
+include("config.php");
+$link=$db;
+//check connection
+if ($link===false){
+die("ERROR:could not connect.".mysqli_connect_error());
+}
+$name=$_post['user name'];
+$cn=$_post['college name'];
+$deg=$_post['degree'];
+$lang=$_post['language'];
+$eml=$_post['email'];
+$pwd=$_post[password'];
+$rpwd=$_post['repeat password'];
+//Attempt insert query execution
+$sql="INSERT INTO REGISTER('user name','college name','degree','language','email','password','repeat password');
+values($user name,$college name,$degree,$language,$email,$password,$repeat password);
+if(mysqli_query($db.$sql)){
+echo"Records added successfully";
+}else{
+echo"ERROR:could not able to execute $sql.".mysqli_error($link);
+}
+//close connection
+mysqli_close($link);
+?>
